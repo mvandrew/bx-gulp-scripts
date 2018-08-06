adc9b9e2df3ccdc25597fa9ac75397aaa107850f
Andrey Mishchenko <msav@msav.ru>
2018-08-06 20:25:11 +0300

    Formated the config source code.

--- config/index.default.js
+++ config/index.default.js
@@ -7,8 +7,8 @@ const templateName  = "templateName";
 module.exports      = {
     dev_mode:           true,
     assets:             assetsPath,
-    template:           path.join(assetsPath, "templates", templateName),
-    template_dest:      path.join(rootPath, "local", "templates", templateName),
-    bower:              path.join(rootPath, "bower_components"),
-    node:               path.join(rootPath, "node_modules")
+    template:           path.join(assetsPath,   "templates",        templateName),
+    template_dest:      path.join(rootPath,     "local",            "templates", templateName),
+    bower:              path.join(rootPath,     "bower_components"),
+    node:               path.join(rootPath,     "node_modules")
 };
