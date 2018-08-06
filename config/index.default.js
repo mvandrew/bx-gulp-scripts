bfb9710629f8faf1a8aeaca675d0a34996301664
Andrey Mishchenko <msav@msav.ru>
2018-08-06 20:09:36 +0300

    Init

---
+++ config/index.default.js
@@ -0,0 +1,14 @@
+const path          = require("path");
+
+const rootPath      = path.join(__dirname, "../../..");
+const assetsPath    = path.join(__dirname, "../..", "assets");
+const templateName  = "templateName";
+
+module.exports      = {
+    dev_mode:           true,
+    assets:             assetsPath,
+    template:           path.join(assetsPath, "templates", templateName),
+    template_dest:      path.join(rootPath, "local", "templates", templateName),
+    bower:              path.join(rootPath, "bower_components"),
+    node:               path.join(rootPath, "node_modules")
+};
